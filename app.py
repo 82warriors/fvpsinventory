a7f6b88f9616f284fd75931259be6721a77e5c89
82warriors <simjs1982@gmail.com>
2026-04-02 10:14:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="FVPS Inventory System", layout="wide")
+
+st.title("📊 FVPS Inventory & Patching System")
+st.write("Use the sidebar to navigate")

20b11a56ae16e0333aa022f97a5d3e7b7e9c061b
82warriors <simjs1982@gmail.com>
2026-04-02 14:45:01 +0800

    Update 2_Patching_report.py

--- pages/2_Patching_report.py
+++ pages/2_Patching_report.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("🛠 Patching Report")
 
 url = "https://docs.google.com/spreadsheets/d/1zvwKzIEbvQEEgbcqcyp9WP0IfguSaHm2G67ZAeuiSOE/export?format=csv"
-df = pd.read_csv(url)
+df = pd.read_csv(BASE, header=3)
 
 st.dataframe(df)
 
